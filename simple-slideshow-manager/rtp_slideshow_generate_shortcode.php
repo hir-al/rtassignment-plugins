c77d5de35397d8f4af6c682ed3fbdb55d426ff14
hir-al <hiral.makwana@rocketmail.com>
2014-06-04 22:49:51 +0530

    Applied structured format

--- simple-slideshow-manager/rtp_slideshow_generate_shortcode.php
+++ simple-slideshow-manager/rtp_slideshow_generate_shortcode.php
@@ -1,15 +1,18 @@
-<?php	
+<?php
 $rtp_slideshow_misc_hide_advert = get_option('rtp_slideshow_misc_hide_advert');
-if($rtp_slideshow_misc_hide_advert == "")
-{
-$rtp_slideshow_misc_hide_advert = "no";
+
+if ($rtp_slideshow_misc_hide_advert == "") {
+	$rtp_slideshow_misc_hide_advert = "no";
 }
-$rtp_slideshow_imageupload_complete_data=unserialize(get_option('rtp_slideshow_imageupload_complete_data'));
-if($rtp_slideshow_imageupload_complete_data=="")
-{
-	$rtp_slideshow_imageupload_complete_data=array();
+
+$rtp_slideshow_imageupload_complete_data = unserialize(get_option('rtp_slideshow_imageupload_complete_data'));
+
+if ($rtp_slideshow_imageupload_complete_data == "") {
+	$rtp_slideshow_imageupload_complete_data = array();
 }
-//print_r($rtp_slideshow_imageupload_complete_data);
+
+// print_r($rtp_slideshow_imageupload_complete_data);
+
 ?>
 <script type="text/javascript">
 function rtp_generate()
@@ -27,7 +30,8 @@ rtp_height = document.getElementById("rtp_shortcode_gall_height").value;
 } else
 {
 rtp_height = "";
-alert('<?php _e('Height needs to be a number','simple-slideshow-manager'); ?>');
+alert('<?php
+_e('Height needs to be a number', 'simple-slideshow-manager'); ?>');
 }
 if(!isNaN(document.getElementById("rtp_shortcode_gall_width").value))
 {
@@ -35,7 +39,8 @@ rtp_width = document.getElementById("rtp_shortcode_gall_width").value;
 } else
 {
 rtp_width = "";
-alert('<?php _e('Width needs to be a number','simple-slideshow-manager'); ?>');
+alert('<?php
+_e('Width needs to be a number', 'simple-slideshow-manager'); ?>');
 }
 jQuery("#rtp_shortcode").show();
 if(rtp_height == "")
@@ -64,51 +69,63 @@ else
 
 <div class="wrap">
 	<?php
-	echo "<h2>" . __( 'Generate Shortcode', 'simple-slideshow-manager' ) . "</h2>"; 
-	?>
+echo "<h2>" . __('Generate Shortcode', 'simple-slideshow-manager') . "</h2>";
+?>
 	<div style="padding:8px;width:99%;margin-top:8px;" class="widefat">
 		<table style="border:0px;" id="rtp_short_gen" cellspacing="0">
 			<tr>
-				<td> <?php _e('Select Gallery','simple-slideshow-manager'); ?></td>
+				<td> <?php
+_e('Select Gallery', 'simple-slideshow-manager'); ?></td>
 				<td>
 				<select id="rtp_shortcode_gallery" name="rtp_shortcode_gallery">	
 				<?php
-				foreach ($rtp_slideshow_imageupload_complete_data as $key => $value)
-				{
-				?>
-				<option value="<?php echo $key; ?>"><?php echo $key; ?></option>
+
+foreach($rtp_slideshow_imageupload_complete_data as $key => $value) {
+?>
+				<option value="<?php
+	echo $key; ?>"><?php
+	echo $key; ?></option>
 				<?php
-				}
-				?>
+}
+
+?>
 				</select></td>
 			</tr>
 			<tr>
-				<td><?php _e('Height','simple-slideshow-manager'); ?></td>
+				<td><?php
+_e('Height', 'simple-slideshow-manager'); ?></td>
 				<td>
 				<input type="text" name="rtp_shortcode_gall_height" id="rtp_shortcode_gall_height" value=""/>
 				<select id="rtp_height_type" name="rtp_height_type">
-				<option value="px"><?php _e('px','simple-slideshow-manager'); ?></option>
-				<option value="%"><?php _e('%','simple-slideshow-manager'); ?></option>
+				<option value="px"><?php
+_e('px', 'simple-slideshow-manager'); ?></option>
+				<option value="%"><?php
+_e('%', 'simple-slideshow-manager'); ?></option>
 				</select>
 				</td>
 			</tr>
 			<tr>
 				<td>
-				<?php _e('Width','simple-slideshow-manager'); ?></td><td>
+				<?php
+_e('Width', 'simple-slideshow-manager'); ?></td><td>
 				<input type="text" name="rtp_shortcode_gall_width" id="rtp_shortcode_gall_width" value=""/>
 				<select id="rtp_width_type" name="rtp_width_type">
-				<option value="px"><?php _e('px','simple-slideshow-manager'); ?></option>
-				<option value="%"><?php _e('%','simple-slideshow-manager'); ?></option>
+				<option value="px"><?php
+_e('px', 'simple-slideshow-manager'); ?></option>
+				<option value="%"><?php
+_e('%', 'simple-slideshow-manager'); ?></option>
 				</select>
 				</td>
 			</tr>
 			<tr>
-				<td></td><td>	<a id="rtp_slideshow_generate_shortcode" class="manage_gallery"  href="#" style="background: none repeat scroll 0px 0px lightseagreen; color: white; padding: 0px 5px 1px; text-decoration: none; border: 1px solid gray;" alt = "Click to Manage this Gallery" onclick = "rtp_generate()"><?php _e('Generate Shortcode','simple-slideshow-manager'); ?></a></td>
+				<td></td><td>	<a id="rtp_slideshow_generate_shortcode" class="manage_gallery"  href="#" style="background: none repeat scroll 0px 0px lightseagreen; color: white; padding: 0px 5px 1px; text-decoration: none; border: 1px solid gray;" alt = "Click to Manage this Gallery" onclick = "rtp_generate()"><?php
+_e('Generate Shortcode', 'simple-slideshow-manager'); ?></a></td>
 			</tr>
 			<tr>
 				<td colspan="2">
 				<div id="rtp_shortcode" style = "display:none">
-				<b><?php _e('Shortcode:-','simple-slideshow-manager'); ?></b>
+				<b><?php
+_e('Shortcode:-', 'simple-slideshow-manager'); ?></b>
 				<input type="text" name="rtp_shortcode_display" id="rtp_shortcode_display" value="" readonly size="60" onClick="javascript:this.focus();this.select();">
 				</div>
 				</td>
