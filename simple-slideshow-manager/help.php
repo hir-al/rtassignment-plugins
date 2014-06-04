8fa4cfa89623af762ffc2f02c6f1cb72bcdcef13
hir-al <hiral.makwana@rocketmail.com>
2014-06-04 22:48:50 +0530

    Applied structured format

--- simple-slideshow-manager/help.php
+++ simple-slideshow-manager/help.php
@@ -1,14 +1,18 @@
 <div id="rtp_help_page">
 <?php
 $rtp_slideshow_misc_hide_advert = get_option('rtp_slideshow_misc_hide_advert');
-if($rtp_slideshow_misc_hide_advert == "")
-{
-$rtp_slideshow_misc_hide_advert = "no";
+
+if ($rtp_slideshow_misc_hide_advert == "") {
+	$rtp_slideshow_misc_hide_advert = "no";
 }
+
 ?>
-<h2><?php _e('Simple Slideshow Manager - Wordpress Plugin - Help/Support','simple-slideshow-manager'); ?></h2>
+<h2><?php
+_e('Simple Slideshow Manager - Wordpress Plugin - Help/Support', 'simple-slideshow-manager'); ?></h2>
 
-<p><?php _e('Thank you for using Simple Slideshow Manager For Your Wordpress Slideshow Need.','simple-slideshow-manager'); ?></p>
+<p><?php
+_e('Thank you for using Simple Slideshow Manager For Your Wordpress Slideshow Need.', 'simple-slideshow-manager'); ?></p>
 
-<h3><a href="http://clients.acurax.com/link.php?id=13" target="_blank"><?php _e('Click here to open the FAQ and Help Page','simple-slideshow-manager'); ?></a></h3>
+<h3><a href="http://clients.acurax.com/link.php?id=13" target="_blank"><?php
+_e('Click here to open the FAQ and Help Page', 'simple-slideshow-manager'); ?></a></h3>
 </div> <!-- rtp_help_page -->
